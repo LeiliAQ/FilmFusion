83bb21edd4da948dab9473030644be022f360de6
DESKTOP-6E2TF2P\leili <leiliqajar22@gmail.com>
2024-08-10 23:37:35 +1000

    Error handler for image MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default function MovieCard({ movie }) {
+  // error handler
+  const imageErrorHandler = (event) => {
+    event.target.src = "images/default.jpg";
+  };
   return (
     <div key={movie.id} className="movie-card">
-      <img src={`./images/${movie.image}`} alt="" className="movie_image" />
+      <img src={`./images/${movie.image}`} alt="" className="movie_image" 
+      onError={imageErrorHandler}
+      />
       <div className="movie-card-info">
         <h3 className="movie-card-info">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
