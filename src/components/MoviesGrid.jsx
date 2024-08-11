62a2232d6ccd4ba6fb68a274cff761b2887e7880
DESKTOP-6E2TF2P\leili <leiliqajar22@gmail.com>
2024-08-11 12:01:52 +1000

    search input added to MoviesGrid

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -4,18 +4,29 @@ import './MovieCard'
 import MovieCard from './MovieCard'
 export default function MoviesGrid() {
     const [movies,setMovies]=useState([])
+    const[searchTerm,setsearchterm]=useState('')
     useEffect(()=>{
         fetch("./movies.json")
         .then(res=>res.json())
         .then(data=>setMovies(data))
         
     },[])//componentDidMount
+    const doSearch=(event)=>{
+      setsearchterm(event.target.value)
+    }
+    const filteredMovies=movies.filter(movie=>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
     return(
         
         <div>
+            <input type='text' className='search-input' placeholder='Search Movies...'
+            value={searchTerm}
+            onChange={doSearch}
+            />
             <div className="movies-grid">
                {
-                movies.map(movie=>(
+                filteredMovies.map(movie=>(
                     <MovieCard movie={movie} key={movie.id}/>
                 ))
                }
